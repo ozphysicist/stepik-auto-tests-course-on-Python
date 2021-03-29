c378b4ea6d7e7afb7fd69564aafe730e6ccca09d
OZ <zhdanovao@outlook.com>
2021-03-29 16:26:37 +0300

    update test_items.py

--- section3_lang/test_items.py
+++ section3_lang/test_items.py
@@ -4,4 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_add_button_title(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form button")
+    time.sleep(30)
+    busket_button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    assert len(busket_button) == 1, "'Add to basket' button isn't unique or not found"
